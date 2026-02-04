313bb2544a8d5da0a627c071333faae5c839414a
Theeojeong <wogusto13@gmail.com>
2026-02-04 23:56:17 +0900

    260204 update

--- 코테_문제/섹션_3/2. 숫자만 추출/AA.py
+++ 코테_문제/섹션_3/2. 숫자만 추출/AA.py
@@ -1,13 +1,46 @@
 import sys
+import os
+
+BASE = os.path.dirname(__file__)
+sys.stdin = open(os.path.join(BASE, "in1.txt"), "rt")
 
 x = input()
 
-digits = [i for i in x if i.isdigit()]
-number = int("".join(digits))
-print(number)
+y = []
 
-count=0
-for i in range(1, number+1):
-    if number%i == 0:
-        count = count + 1
-print(count)
+num = 0
+        # g0en2T0s8eSoft
+        # 1000
+for i in x:
+    if i.isdecimal():
+        num = num * 10 + int(i)
+print(num)
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# x = input()
+
+# digits = [i for i in x if i.isdigit()]
+# number = int("".join(digits))
+# print(number)
+
+# count=0
+# for i in range(1, number+1):
+#     if number%i == 0:
+#         count = count + 1
+# print(count)
