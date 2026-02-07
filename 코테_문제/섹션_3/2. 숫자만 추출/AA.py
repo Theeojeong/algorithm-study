e61d636847b61050d2a4dadb9b3df6156165fecd
Theeojeong <wogusto13@gmail.com>
2026-02-07 23:27:07 +0900

    update 260207

--- 코테_문제/섹션_3/2. 숫자만 추출/AA.py
+++ 코테_문제/섹션_3/2. 숫자만 추출/AA.py
@@ -1,16 +1,17 @@
 import sys
 import os
 
-BASE = os.path.dirname(__file__)
-sys.stdin = open(os.path.join(BASE, "in1.txt"), "rt")
+# BASE = os.path.dirname(__file__)
+# sys.stdin = open(os.path.join(BASE, "in1.txt"), "rt")
 
 x = input()
+
 
 y = []
 
 num = 0
-        # g0en2T0s8eSoft
-        # 1000
+# g0en2T0s8eSoft
+# 1000
 for i in x:
     if i.isdecimal():
         num = num * 10 + int(i)
@@ -18,24 +19,11 @@ print(num)
 
 cnt = 0
 
+for i in range(1, num + 1):
+    if num%i == 0:
+        cnt = cnt + 1
 
-
-
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(cnt)
 
 
 # x = input()
--- 코테_문제/섹션_3/3. 카드 역배치/AA.py
+++ 코테_문제/섹션_3/3. 카드 역배치/AA.py
@@ -1,13 +1,59 @@
 import sys
+import os
+
+# BASE = os.path.dirname(__file__)
+# sys.stdin = open(os.path.join(BASE, "in1.txt"), "rt")
 
 card = list(range(1, 21))
 
 for i in range(10):
-    a, b = map(int, input().split())
-    x = card[a-1:b]
-    x = x[::-1]
-    card[a-1:b] = x
-    # print(card)
-for j in card:
-    print(j, end=" ")
-    
+    x, y = map(int, input().split())
+    # print(x, y)
+    # 5, 10 card[4:10] 5678910
+    z = card[x-1:y]
+    z = z[::-1]
+
+    card[x-1:y] = z
+
+for i in card:
+    print(i, sep=" ", end=" ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# card = list(range(1, 21))
+
+# for i in range(10):
+#     a, b = map(int, input().split())
+#     x = card[a-1:b]
+#     x = x[::-1]
+#     card[a-1:b] = x
+#     # print(card)
+# for j in card:
+#     print(j, end=" ")
